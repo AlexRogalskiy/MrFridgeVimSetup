4abf975503a5c777ed8e8af697818f0c803d7661
Gabriel Reimers <g.a.reimers@gmail.com>
2013-12-27 13:34:52 +0100

    - adds install_all_plugins script - adds info to set caps-lock as esc

---
+++ install_all_plugins.sh
@@ -0,0 +1,11 @@
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-sensible.git
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/xolox/vim-misc.git
+git clone https://github.com/xolox/vim-easytags.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
+git clone https://github.com/techlivezheng/vim-plugin-minibufexpl.git
+git clone https://github.com/kien/ctrlp.vim.git
+
+
